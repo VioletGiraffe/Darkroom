56ec259b16aad7e6d41f98173580d3478ec025be
github-action-benchmark <github@users.noreply.github.com>
2026-07-05 21:09:52 +0000

    add Darkroom metrics (customSmallerIsBetter) benchmark result for 57d04d9b523c63cbe25671aedb4503e641f60432

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783285792148,
+  "repoUrl": "https://github.com/VioletGiraffe/Darkroom",
+  "entries": {
+    "Darkroom metrics": [
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "57d04d9b523c63cbe25671aedb4503e641f60432",
+          "message": "CI.yml: tracking metrics over time via gh-pages",
+          "timestamp": "2026-07-06T00:05:21+03:00",
+          "tree_id": "72a50db6a2139358e7db8c345df92f65fe52d22d",
+          "url": "https://github.com/VioletGiraffe/Darkroom/commit/57d04d9b523c63cbe25671aedb4503e641f60432"
+        },
+        "date": 1783285789284,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 7256,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 3343,
+            "unit": "LOC"
+          },
+          {
+            "name": "Darkroom.exe size",
+            "value": 1081.5,
+            "unit": "KB"
+          }
+        ]
+      }
+    ]
+  }
+}
